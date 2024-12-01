0fd2b865855d07317b608a1f3d016370ff6b4c83
Yasin Çoban <cobanyasin1998@gmail.com>
2024-12-01 23:37:48 +0300

    fixed namespaces

--- src/PharmacyService/Core/PharmacyService.Application/Features/Pharmacy/Queries/GetAll/RequestDto/ExampleCustomFilter.cs
+++ src/PharmacyService/Core/PharmacyService.Application/Features/Pharmacy/Queries/GetAll/RequestDto/ExampleCustomFilter.cs
@@ -1,94 +1,96 @@
 ﻿using Coban.Application.Requests.Filter.Dynamic.QueryRequest.Dto;
 
+namespace PharmacyService.Application.Features.Pharmacy.Queries.GetAll.RequestDto;
+
 public class ExampleCustomFilter
 {
     public List<FilterGroup> ExampleFilter()
     {
         var filters = new List<FilterGroup>()
+    {
+        new FilterGroup()
         {
-            new FilterGroup()
+            IntergroupLogic = "or",
+            InterfilterOperator = "and",
+            Filters = new List<Filter>() {
+                new Filter()
+                {
+                    Member = "LicenseNumber",
+                    FilterValue = "965-187-3464",
+                    FilterOperator = "equals"
+                },
+                new Filter()
+                {
+                    Member = "LicenseNumber",
+                    FilterValue = "",
+                    FilterOperator = "isnotnull"
+                }
+            }
+        },
+        new FilterGroup()
+        {
+            IntergroupLogic = "or",
+            InterfilterOperator = "and",
+            Filters = new List<Filter>()
             {
-                IntergroupLogic = "or",
-                InterfilterOperator = "and",
-                Filters = new List<Filter>() {
-                    new Filter()
-                    {
-                        Member = "LicenseNumber",
-                        FilterValue = "965-187-3464",
-                        FilterOperator = "equals"
-                    },
-                    new Filter()
-                    {
-                        Member = "LicenseNumber",
-                        FilterValue = "",
-                        FilterOperator = "isnotnull"
-                    }
+                new Filter()
+                {
+                    Member = "Name",
+                    FilterValue = "string",
+                    FilterOperator = "notequals"
+                },
+                new Filter()
+                {
+                    Member = "LicenseNumber",
+                    FilterValue = "965-",
+                    FilterOperator = "startswith"
                 }
             },
-            new FilterGroup()
+            ChildGroups = new List<FilterGroup>()
             {
-                IntergroupLogic = "or",
-                InterfilterOperator = "and",
-                Filters = new List<Filter>()
+                new FilterGroup()
                 {
-                    new Filter()
+                    IntergroupLogic = "and",
+                    InterfilterOperator = "or",
+                    Filters = new List<Filter>()
                     {
-                        Member = "Name",
-                        FilterValue = "string",
-                        FilterOperator = "notequals"
-                    },
-                    new Filter()
-                    {
-                        Member = "LicenseNumber",
-                        FilterValue = "965-",
-                        FilterOperator = "startswith"
+                        new Filter()
+                        {
+                            Member = "licenseNumber",
+                            FilterValue = "965-304-2032",
+                            FilterOperator = "equals"
+                        },
+                        new Filter()
+                        {
+                            Member = "licenseNumber",
+                            FilterValue = "965-412-5388",
+                            FilterOperator = "equals"
+                        }
                     }
                 },
-                ChildGroups = new List<FilterGroup>()
+                new FilterGroup()
                 {
-                    new FilterGroup()
+                    IntergroupLogic = "and",
+                    InterfilterOperator = "or",
+                    Filters = new List<Filter>()
                     {
-                        IntergroupLogic = "and",
-                        InterfilterOperator = "or",
-                        Filters = new List<Filter>()
+                        new Filter()
                         {
-                            new Filter()
-                            {
-                                Member = "licenseNumber",
-                                FilterValue = "965-304-2032",
-                                FilterOperator = "equals"
-                            },
-                            new Filter()
-                            {
-                                Member = "licenseNumber",
-                                FilterValue = "965-412-5388",
-                                FilterOperator = "equals"
-                            }
-                        }
-                    },
-                    new FilterGroup()
-                    {
-                        IntergroupLogic = "and",
-                        InterfilterOperator = "or",
-                        Filters = new List<Filter>()
+                            Member = "licenseNumber",
+                            FilterValue = "965",
+                            FilterOperator = "equals"
+                        },
+                        new Filter()
                         {
-                            new Filter()
-                            {
-                                Member = "licenseNumber",
-                                FilterValue = "965",
-                                FilterOperator = "equals"
-                            },
-                            new Filter()
-                            {
-                                Member = "licenseNumber",
-                                FilterValue = "5388",
-                                FilterOperator = "endswith"
-                            }
+                            Member = "licenseNumber",
+                            FilterValue = "5388",
+                            FilterOperator = "endswith"
                         }
                     }
                 }
             }
-        };
+        }
+    };
 
         return filters;
     }
